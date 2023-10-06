6ca9a9aef2449bc18d719154229c3f2f410347db
Bluzzi <camilledugas.pro@gmail.com>
2023-10-06 12:39:27 +0200

    fix(PipelineWatch): get branch dynamically

--- Core/Functions/PipelineWatch.ps1
+++ Core/Functions/PipelineWatch.ps1
@@ -1,7 +1,5 @@
-$currentBranch = git rev-parse --abbrev-ref HEAD
-
 function pw([string] $branch) {
-  $branchName = ($branch) ? "$branch" : "$currentBranch"
+  $branchName = ($branch) ? "$branch" : (git rev-parse --abbrev-ref HEAD)
 
   we NODE_OPTIONS=--no-warnings pipeline-watch -p meta -b $branchName
 }
