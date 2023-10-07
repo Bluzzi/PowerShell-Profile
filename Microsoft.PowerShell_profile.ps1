280190b4598225530066cb6dd2309d77d55c1bbe
Bluzzi <camilledugas.pro@gmail.com>
2023-10-07 16:15:17 +0200

    refactor(Microsoft.PowerShell_profile.ps1): reorganize auto-loading of commands, extensions, and configs for better readability and maintainability

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,24 @@
-. "$PSScriptRoot\Core\Commands\Get-AbsolutePath\Get-AbsolutePath.Cmd.ps1"
-. "$PSScriptRoot\Core\Commands\Sudo\Sudo.Cmd.ps1"
+# Auto-load commands:
+$CommandsPath = "$PSScriptRoot\Core\Commands"
 
-. "$PSScriptRoot\Core\Functions\Function.ps1"
-. "$PSScriptRoot\Core\Functions\Color.ps1"
-. "$PSScriptRoot\Core\Functions\Base64.ps1"
-. "$PSScriptRoot\Core\Functions\Git.ps1"
-. "$PSScriptRoot\Core\Functions\Env.ps1"
-. "$PSScriptRoot\Core\Functions\Module.ps1"
-. "$PSScriptRoot\Core\Functions\PowerShell.ps1"
-. "$PSScriptRoot\Core\Functions\Admin.ps1"
-. "$PSScriptRoot\Core\Functions\PipelineWatch.ps1"
+foreach ($FolderPath in Get-ChildItem -Path $CommandsPath -Directory) {
+  $FolderName = Split-Path -Path $FolderPath -Leaf
 
-. "$PSScriptRoot\Core\Extensions\Zoxide.ps1"
-. "$PSScriptRoot\Core\Extensions\PoshGit.ps1"
-. "$PSScriptRoot\Core\Extensions\NPM.ps1"
+  if (!(Test-Path -Path "$FolderPath\$FolderName.Cmd.ps1" -PathType Leaf)) { return }
 
-. "$PSScriptRoot\Core\Configs\PSReadLine.ps1"
-. "$PSScriptRoot\Core\Configs\Alias.ps1"
-. "$PSScriptRoot\Core\Configs\Env.ps1"
+  . "$FolderPath\$FolderName.Cmd.ps1"
+}
+
+foreach ($FilePath in Get-ChildItem -Path "$PSScriptRoot\Core\Functions" -File) {
+  . $FilePath
+}
+
+# Auto-load extensions:
+foreach ($FilePath in Get-ChildItem -Path "$PSScriptRoot\Core\Extensions" -File) {
+  . $FilePath
+}
+
+# Auto-load configs:
+foreach ($FilePath in Get-ChildItem -Path "$PSScriptRoot\Core\Configs" -File) {
+  . $FilePath
+}
