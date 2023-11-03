826c40145a1720ae84ec7b5d98d80319ed602363
Bluzzi <camilledugas.pro@gmail.com>
2023-11-03 02:30:50 +0100

    feat(Get-First-Commit.Cmd.ps1): add new PowerShell script to retrieve the first commit of the current git branch
    The `Get-First-Commit.Cmd.ps1` script is added to the `Core/Commands/Get-First-Commit` directory. This script provides a function `Get-First-Commit` that displays the hash of the first commit of the current git branch.
    
    The function retrieves the main branch name from the remote origin and the current branch name using git commands. It then calculates the difference between the main branch and the current branch. The script uses `git log` to retrieve the commit history in reverse order and selects the hash of the first commit. The hash is then displayed using `Write-Host` and returned as the result of the function.

---
+++ Core/Commands/Get-First-Commit/Get-First-Commit.Cmd.ps1
@@ -0,0 +1,15 @@
+<#
+  .SYNOPSIS
+  Display the first commit of the current git branch.
+#>
+function Get-First-Commit {
+  $MainBranch = (git remote show origin | Select-String "HEAD branch" | ForEach-Object { $_ -replace "HEAD branch: ", "" }).Trim()
+  $CurrentBranch = git branch --show-current
+
+  $BranchsDiff = "$MainBranch..$CurrentBranch"
+
+  $FirstCommitHash = git log --oneline --reverse $BranchsDiff | ForEach-Object { $_.Split(" ")[0] } | Select-Object -First 1 
+
+  Write-Host $FirstCommitHash
+  return $FirstCommitHash
+}
