c24f825e4f5fe4cb4dc10c4f45a28980a790fbfc
Bluzzi <camilledugas.pro@gmail.com>
2024-05-23 19:19:53 +0200

    feat(Base64.ps1): add Convert-ToBase64File function to encode file content to Base64
    The new function `Convert-ToBase64File` takes an input file path and an output file path, encodes the content of the input file to Base64, and writes the encoded content to the output file. This feature was added to facilitate file encoding operations within scripts.

--- Core/Functions/Base64.ps1
+++ Core/Functions/Base64.ps1
@@ -17,3 +17,34 @@ function atob {
 
   [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($string))
 }
+
+function Convert-ToBase64File {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$InputFilePath,
+
+        [Parameter(Mandatory=$true)]
+        [string]$OutputFilePath
+    )
+
+    # Vérifier si le fichier d'entrée existe
+    if (-Not (Test-Path $InputFilePath)) {
+        Write-Error "Le fichier spécifié n'existe pas : $InputFilePath"
+        return
+    }
+
+    try {
+        # Lire le contenu du fichier d'entrée
+        $content = Get-Content -Path $InputFilePath -Raw
+        
+        # Encoder le contenu en Base64
+        $encodedContent = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($content))
+        
+        # Écrire le contenu encodé dans le fichier de sortie
+        Set-Content -Path $OutputFilePath -Value $encodedContent
+        Write-Host "Le fichier a été encodé et sauvegardé : $OutputFilePath"
+    }
+    catch {
+        Write-Error "Une erreur s'est produite lors de l'encodage ou de l'écriture du fichier : $_"
+    }
+}
